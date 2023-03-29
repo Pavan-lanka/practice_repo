13efe8a036917179694b6506a6eb4a1ffa666233
“Pavan-lanka” <pavan.lanka@azistaindustries.com>
2023-03-29 13:47:45 +0530

    First commit

--- station_model.py
+++ station_model.py
@@ -1,48 +1,32 @@
-import metpy.plots as plots
-from metpy.units import units
-import matplotlib.pyplot as plt
+import metpy.io as mt
+# import st_plot as st
+import xarray as xr
 import pandas as pd
-import meteostat as mt
-from datetime import datetime as dt
+
+#
+# def __main__():
+#     i = st.FetchData.get_input_date_time()
 
 
-class FetchData:
-    # __init__ takes Station ID of  the required Station Model
-    def __init__(self, station_id: str , start : dt, end : dt, period:str ):
-        self.station_id = station_id
-        self.start = start
-        self.end = end
-        self.period = period
+def custom_file_read(path_to_file):
 
-    def fetch_station_data(self):
-        """ to get Station data
-            start = datetime(starting time period of the required data) ex. datetime(
-            end = ending time period of the required data
-            period = Frequency of the data period i.e(Hourly, Monthly, Daily)"""
-        if self.period.lower() == 'hourly':
-            if self.start > self.end:
-                print('Enter Valid hours as you provided same start date time and end date time')
-            else:
-                data = mt.Hourly(self.station_id, self.start, self.end)
-                data = data.fetch()
-                return data
-        elif self.period.lower() == 'daily':
-            if self.start > self.end or self.start == self.end:
-                print('Enter Valid days for getting Daily data')
-            else:
-                data = mt.Daily(self.station_id, self.start, self.end)
-                data = data.fetch()
-                return data
-        elif self.period.lower() == 'monthly':
-            if self.start > self.end or self.start == self.end:
-                print('Enter Valid months in Date,Time for fetching monthly data')
-            else:
-                data = mt.Monthly(self.station_id, self.start, self.end)
-                data = data.fetch()
-                return data
-        else:
-            return print('The data period frequency is not valid')
-
-class CustomData:
-    def path_to_file(self,PATH_TO_FILE):
-        self.PATH_TO_FILE = PATH_TO_FILE
+    supported_types = ['nc', 'xml', 'txt', 'csv']
+    extension_read = {'xml' : pd.read_xml,
+                      'nc' : xr.open_dataset,
+                      'txt' : mt.parse_metar_file,
+                      'csv' : pd.read_csv
+    }
+    extension = path_to_file[path_to_file.rfind('.'):][1:]
+    if extension not in supported_types:
+        raise TypeError(f'Supported file formats are {supported_types}')
+    elif extension == 'nc':
+        data = extension_read['nc'](filename_or_obj= path_to_file, engine="netcdf4")
+        data = data.metpy.parse_cf()
+        data = data.to_dataframe()
+        data = data.reset_index()
+    else:
+        data = extension_read[extension](path_to_file)
+    d_parameters = list(data.columns.values)
+    return data, d_parameters
+a, b = custom_file_read(r"/home/hp/metar_vij.txt")
+print(a)
