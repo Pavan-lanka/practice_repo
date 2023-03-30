e45230c91757c929c6cc72ce11f1869dcfa4377a
“Pavan-lanka” <pavan.lanka@azistaindustries.com>
2023-03-30 18:18:41 +0530

    First commit

--- station_model.py
+++ station_model.py
@@ -76,7 +76,7 @@ class StationModelPlot:
     @staticmethod
     def get_input_data(self):
         ts_str = input("Enter a timestamp in the format 'YYYY-MM-DD HH:MM:SS': ")
-        ts = datetime.strptime(ts_str, '%Y-%m-%d %H:%M:%S')
+        ts = dt.strptime(ts_str, '%Y-%m-%d %H:%M:%S')
         return ts
 
     @staticmethod
@@ -140,103 +140,103 @@ class StationModelPlot:
 
 
 @dataclass()
-class StationModel(StationModelPlot):
-    fig, ax = plt.subplots(figsize=(10, 10))
-    sp = StationPlot(ax, 0, 0, fontsize=13, spacing=25)
-    ax.set_xlim(-8, 8)
-    ax.set_ylim(-8, 8)
-    ax.set_title('Station Model')
-    station_circle = patches.Circle((0, 0), radius=7, lw=1, edgecolor='k', facecolor='w')
-    ax.add_patch(station_circle)
-    data = {
-        # 'temperature': None,
-        # 'dew_point_temperature': None,
-        # 'wind_speed': None,
-        # 'wind_direction': None,
-        # 'cloud_height': None,
-        # 'pressure': None,
-        # 'high_cloud': None,
-        # 'mid_cloud': None,
-        # 'low_cloud': None,
-        # 'sky_cover': None,
-        # 'visibility_distance': None,
-        # 'present_weather': None,
-        # 'past_weather': None,
-        # 'pressure_tendency': None,
-        # 'pressure_change': None,
-        # 'pressure_difference': None,
-        # 'precipitation': None,
-        # 'sky_cover_at_lowest_cloud': None
-    }
+# class StationModel(StationModelPlot):
+#     fig, ax = plt.subplots(figsize=(10, 10))
+#     sp = StationPlot(ax, 0, 0, fontsize=13, spacing=25)
+#     ax.set_xlim(-8, 8)
+#     ax.set_ylim(-8, 8)
+#     ax.set_title('Station Model')
+#     station_circle = patches.Circle((0, 0), radius=7, lw=1, edgecolor='k', facecolor='w')
+#     ax.add_patch(station_circle)
+#     data = {
+#         # 'temperature': None,
+#         # 'dew_point_temperature': None,
+#         # 'wind_speed': None,
+#         # 'wind_direction': None,
+#         # 'cloud_height': None,
+#         # 'pressure': None,
+#         # 'high_cloud': None,
+#         # 'mid_cloud': None,
+#         # 'low_cloud': None,
+#         # 'sky_cover': None,
+#         # 'visibility_distance': None,
+#         # 'present_weather': None,
+#         # 'past_weather': None,
+#         # 'pressure_tendency': None,
+#         # 'pressure_change': None,
+#         # 'pressure_difference': None,
+#         # 'precipitation': None,
+#         # 'sky_cover_at_lowest_cloud': None
+#     }
 
-    def plot_station_model(self):
+#     def plot_station_model(self):
 
 
-        # to add pressure_tendency symbol to the model
-        self.sp.plot_symbol((5, 0), codes=[self.data['pressure_tendency']], symbol_mapper=pressure_tendency,
-                            va='center', ha='center', fontsize=25)
+#         # to add pressure_tendency symbol to the model
+#         self.sp.plot_symbol((5, 0), codes=[self.data['pressure_tendency']], symbol_mapper=pressure_tendency,
+#                             va='center', ha='center', fontsize=25)
 
-        # to add Sky_cover symbol to the model
-        self.sp.plot_symbol((0, 0), codes=[self.data['sky_cover']], symbol_mapper=sky_cover, fontsize=25)
+#         # to add Sky_cover symbol to the model
+#         self.sp.plot_symbol((0, 0), codes=[self.data['sky_cover']], symbol_mapper=sky_cover, fontsize=25)
 
-        # to add pressure to the model
-        self.sp.plot_text((4, 3), text=[str(self.data['pressure']) + ' hPa'], fontsize=13)
+#         # to add pressure to the model
+#         self.sp.plot_text((4, 3), text=[str(self.data['pressure']) + ' hPa'], fontsize=13)
 
-        # to position wind-barb in the center of the model
-        # u = -wind_speed * np.sin(np.radians(wind_direction))
-        # v = -wspd_mps * math.cos(np.radians(wind_direction))
-        self.sp.plot_barb(u=[-(self.data['wind_speed']) * np.sin(np.radians(self.data['wind_direction']))],
-                          v=[-(self.data['wind_speed']) * np.cos(np.radians(self.data['wind_direction']))], length=11)
-        # to add wind speed in knots at the end of the barb
-        self.ax.text(1.5 * np.sin(np.radians(self.data['wind_direction'])),
-                     1.5 * np.cos(np.radians(self.data['wind_direction'])),
-                     str(self.data['wind_speed']) + ' kts',
-                     ha='center', va='bottom', rotation=0, fontsize=10, alpha=0.3)
+#         # to position wind-barb in the center of the model
+#         # u = -wind_speed * np.sin(np.radians(wind_direction))
+#         # v = -wspd_mps * math.cos(np.radians(wind_direction))
+#         self.sp.plot_barb(u=[-(self.data['wind_speed']) * np.sin(np.radians(self.data['wind_direction']))],
+#                           v=[-(self.data['wind_speed']) * np.cos(np.radians(self.data['wind_direction']))], length=11)
+#         # to add wind speed in knots at the end of the barb
+#         self.ax.text(1.5 * np.sin(np.radians(self.data['wind_direction'])),
+#                      1.5 * np.cos(np.radians(self.data['wind_direction'])),
+#                      str(self.data['wind_speed']) + ' kts',
+#                      ha='center', va='bottom', rotation=0, fontsize=10, alpha=0.3)
 
-        # to add height of the cloud base
-        self.sp.plot_text((-2, -5.5), text=[str(self.data['cloud_height'])], fontsize=13)
+#         # to add height of the cloud base
+#         self.sp.plot_text((-2, -5.5), text=[str(self.data['cloud_height'])], fontsize=13)
 
-        # to add dew_point_temperature to the model
-        self.sp.plot_text((-4, 3), text=[str(self.data['dew_point_temperature']) + '°C'], fontsize=13)
+#         # to add dew_point_temperature to the model
+#         self.sp.plot_text((-4, 3), text=[str(self.data['dew_point_temperature']) + '°C'], fontsize=13)
 
-        # to add high_clouds symbol to the model
-        self.sp.plot_symbol((1, 5), codes=[self.data['high_cloud']], symbol_mapper=high_clouds,
-                            va='center', ha='center', fontsize=25)
+#         # to add high_clouds symbol to the model
+#         self.sp.plot_symbol((1, 5), codes=[self.data['high_cloud']], symbol_mapper=high_clouds,
+#                             va='center', ha='center', fontsize=25)
 
-        # to add low_clouds symbol to the model
-        self.sp.plot_symbol((-2, -3.5), codes=[self.data['low_cloud']], symbol_mapper=low_clouds,
-                            va='center', ha='center', fontsize=25)
+#         # to add low_clouds symbol to the model
+#         self.sp.plot_symbol((-2, -3.5), codes=[self.data['low_cloud']], symbol_mapper=low_clouds,
+#                             va='center', ha='center', fontsize=25)
 
-        # to add mid_clouds symbol to the model
-        self.sp.plot_symbol((2, 3), codes=[self.data['mid_cloud']], symbol_mapper=mid_clouds,
-                            va='center', ha='center', fontsize=25)
+#         # to add mid_clouds symbol to the model
+#         self.sp.plot_symbol((2, 3), codes=[self.data['mid_cloud']], symbol_mapper=mid_clouds,
+#                             va='center', ha='center', fontsize=25)
 
-        # to add past_weather symbol to the model
-        self.sp.plot_symbol((2, -3.5), codes=wx_code_map[self.data['past_weather']], symbol_mapper=current_weather,
-                            va='center', ha='center', fontsize=25)
+#         # to add past_weather symbol to the model
+#         self.sp.plot_symbol((2, -3.5), codes=wx_code_map[self.data['past_weather']], symbol_mapper=current_weather,
+#                             va='center', ha='center', fontsize=25)
 
-        # to add precipitation to the model
-        self.sp.plot_text((2, -5.5), text=[str(self.data['precipitation'])], fontsize=13)
+#         # to add precipitation to the model
+#         self.sp.plot_text((2, -5.5), text=[str(self.data['precipitation'])], fontsize=13)
 
-        # to add present_weather symbol to the model
-        self.sp.plot_symbol((-4, 0), codes=wx_code_map[self.data['present_weather']], symbol_mapper=current_weather,
-                            va='center', ha='center', fontsize=25)
+#         # to add present_weather symbol to the model
+#         self.sp.plot_symbol((-4, 0), codes=wx_code_map[self.data['present_weather']], symbol_mapper=current_weather,
+#                             va='center', ha='center', fontsize=25)
 
-        # to add pressure_change to the model
-        self.sp.plot_text((3.2, 0), text=[str(self.data['pressure_change'])], fontsize=13)
+#         # to add pressure_change to the model
+#         self.sp.plot_text((3.2, 0), text=[str(self.data['pressure_change'])], fontsize=13)
 
-        # to add pressure_difference to the model
-        self.sp.plot_text((4, 0), text=[str(self.data['pressure_difference'])], fontsize=13)
+#         # to add pressure_difference to the model
+#         self.sp.plot_text((4, 0), text=[str(self.data['pressure_difference'])], fontsize=13)
 
-        # to add sky_cover_of the lowest cloud to the model
-        self.sp.plot_text((0, -4), text=[str(self.data['sky_cover_at_lowest_cloud'])], fontsize=13)
+#         # to add sky_cover_of the lowest cloud to the model
+#         self.sp.plot_text((0, -4), text=[str(self.data['sky_cover_at_lowest_cloud'])], fontsize=13)
 
-        # to add temperature to the model
-        self.sp.plot_text((-4, 3), text=[str(self.data['temperature']) + '°C'], fontsize=13)
+#         # to add temperature to the model
+#         self.sp.plot_text((-4, 3), text=[str(self.data['temperature']) + '°C'], fontsize=13)
 
-        # to add visibility_distance to the model
-        self.sp.plot_text((-6, 0), text=[str(self.data['visibility_distance']) + 'miles'], fontsize=13)
-        return plt.show()
+#         # to add visibility_distance to the model
+#         self.sp.plot_text((-6, 0), text=[str(self.data['visibility_distance']) + 'miles'], fontsize=13)
+#         return plt.show()
 
 
 
@@ -245,4 +245,4 @@ class StationModel(StationModelPlot):
 # al = add_metpy_logo(fig=fig, x=8, y=8, zorder=5, size='small')
 
 # plt.grid()
-a =  StationModelPlot("")
+# a =  StationModelPlot("")
--- station_plot.py
+++ station_plot.py
@@ -2,83 +2,122 @@ from metpy.plots import *
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-
+import matplotlib.lines as lines
+from metpy.units import units
+import cv2 as cv
 
 fig, ax = plt.subplots(figsize=(10, 10))
 
 ax.set_xlim(-8, 8)
 ax.set_ylim(-8, 8)
 
-
 ax.set_title('Station Model')
 
-
+# Define some sample data
 temperature = 15.6
 dewpoint = 10.2
-wind_speed = int(21 // 1.151)
-wind_direction = 30
+wind_speed = 15
+wind_direction = 0
 pressure = 1010
-cloud_height = '5'
-high_cloud = '*'
-mid_cloud = '*'
-low_cloud = '*'
-# sky_cover = 'O'
-visibility_distance = '0.5'
-present_weather = '*'
-past_weather = '*'
-pressure_tendency = '/'
-pressure_change = '+'
-pressure_difference = '15'
+cloud_height = 5
+high_cloud = ''
+mid_cloud = ''
+low_cloud = ''
+sky_covered = ''
+visibility_distance = 0.5
+present_weather = ''
+past_weather = ''
+press_tend = ''
+press_change = '+'
+pressure_difference = 15
 precipitation = '.45'
 sky_cover_at_lowest_cloud = 8
-
+Station_ID = 'VOHS'
 # Draw the station model
+sp = StationPlot(ax, 0, 0, fontsize=13, spacing=25)
 station_circle = patches.Circle((0, 0), radius=7, lw=1, edgecolor='k', facecolor='w')
-ax.set_aspect(1)
+# ax.set_aspect()
 ax.add_patch(station_circle)
 
 # to plot temperature in the model
-ax.text(-4, 4, str(temperature) + '°C', ha='center', va='center', fontsize=13)
+sp.plot_text((-4, 3), text=[str(temperature) + '°C'], fontsize=13)
 
 # to plot dew_point temperature in the model
-ax.text(-4, -4, str(dewpoint) + '°C', ha='center', va='center', fontsize=13)
+sp.plot_text((-4, -3.5), text=[str(dewpoint) + '°C'], fontsize=13)
 
 # to plot visibility distance in the model
-ax.text(-4, 0, str(visibility_distance) + 'miles', ha='center', va='center', fontsize=13)
+sp.plot_text((-6, 0), text=[str(visibility_distance) + 'miles'], fontsize=13)
 
-sp = StationPlot(ax, 0, 0, clip_on=True, fontsize=13)
+# adding a station plot to insert special figures
+# sp.plot_barb([np.radians(wind_direction)], [wind_speed], length = 12)
+# sp.plot_parameter('
 
+# to position windbarb in the center of the model
+# u = -wind_speed * np.sin(np.radians(wind_direction))
+# v = -wspd_mps * math.cos(np.radians(wind_direction))
 sp.plot_barb(u=[-wind_speed * np.sin(np.radians(wind_direction))],
-             v=[-wind_speed * np.cos(np.radians(wind_direction))], length=12)
+             v=[-wind_speed * np.cos(np.radians(wind_direction))], length=10)
 
-ax.text(3 * np.sin(np.radians(wind_direction)), 3 * np.cos(np.radians(wind_direction)),
-        str(wind_speed) + ' kts', ha='center', va='center', fontsize=13)
+# to add wind speed in knots at the end of the barb
+ax.text(1 * np.sin(np.radians(wind_direction)), 1 * np.cos(np.radians(wind_direction)),
+        str(wind_speed) + ' kts', ha='center', va='bottom', rotation = 0, fontsize=10, alpha=0.3)
+
+#to add Station ID to the model
+sp.plot_text((8, 8), text=['Station_ID: '+ str(Station_ID)], fontsize=13)
+
 # to add pressure to the model
-ax.text(4, 4, str(pressure) + ' hPa', ha='center', va='center', fontsize=13)
-
-# to add pressure_tendency to the model
-ax.text(5, 0, str(pressure_tendency), ha='center', va='center', fontsize=13)
+sp.plot_text((4, 3), text=[str(pressure) + ' hPa'], fontsize=13)
 
 # to add pressure_change to the model
-ax.text(3, 0, str(pressure_change), ha='center', va='center', fontsize=13)
+sp.plot_text((3.2, 0), text=[str(press_change)], fontsize=13)
 
 # to add pressure_difference to the model
-ax.text(4, 0, str(pressure_difference), ha='center', va='center', fontsize=13)
-
-# to add sky_cover to the model
-# ax.text(0, 0, str(sky_cover), ha='center', va='center', fontsize=18)
+sp.plot_text((4, 0), text=[str(pressure_difference)], fontsize=13)
 
 # to add sky_cover_of the lowest cloud to the model
-ax.text(0, -4, str(sky_cover_at_lowest_cloud), ha='center', va='center', fontsize=13)
+sp.plot_text((0, -3.7), text=[str(sky_cover_at_lowest_cloud)], fontsize=13)
 
 # to add height of the cloud base
-ax.text(-2, -4, str(cloud_height), ha='center', va='center', fontsize=13)
+sp.plot_text((-2, -5.5), text=[str(cloud_height)], fontsize=13)
 
 # to add precipitation to the model
-ax.text(2, -6, str(precipitation), ha='center', va='center', fontsize=13)
+sp.plot_text((2, -5.5), text=[str(precipitation)], fontsize=13)
 
+# to add Sky_cover symbol to the model
+sp.plot_symbol((0, 0), codes=[0], symbol_mapper=sky_cover, fontsize=25)
 
-sp.plot_symbol((0, 0), symbol_mapper=sky_cover, codes=[8])
+# to add pressure_tendency symbol to the model
+sp.plot_symbol((5, 0), codes=[2], symbol_mapper=pressure_tendency, va='center', ha='center', fontsize=25)
 
-plt.grid()
-plt.show()
+# to add low_clouds symbol to the model
+sp.plot_symbol((-2, -3.5), codes=[1], symbol_mapper=low_clouds, va='center', ha='center', fontsize=25)
+
+# to add mid_clouds symbol to the model
+sp.plot_symbol((2, 3), codes=[8], symbol_mapper=mid_clouds, va='center', ha='center', fontsize=25)
+
+# to add high_clouds symbol to the model
+sp.plot_symbol((1, 5), codes=[1], symbol_mapper=high_clouds, va='center', ha='center', fontsize=25)
+
+# to add current_weather symbol to the model
+sp.plot_symbol((-4, 0), codes = [wx_code_map['TS']], symbol_mapper=current_weather, va='center', ha='center', fontsize=25)
+
+# to add past_weather symbol to the model
+sp.plot_symbol((2, -3.5), codes=[wx_code_map['TS']], symbol_mapper=current_weather, va='center', ha='center', fontsize=25)
+
+# adding metpy logo at the corner
+# al = add_metpy_logo(fig=fig, x=8, y=8, zorder=5, size='small')
+
+# plt.grid()
+# plt.show()
+path = '/home/hp/PycharmProjects/station_model/'
+name = 'Station_Model.jpeg'
+plt.savefig(path+name, dpi= 100)
+cv.waitKey(5)
+print(wx_code_map['TS'])
+image = cv.imread(path+name)
+cv.imshow(name.rstrip('.jpeg'), image)
+k = cv.waitKey(0) & 0xFF
+print(k)
+if k == 233 :  # close on ESC key
+    cv.destroyAllWindows()
+
