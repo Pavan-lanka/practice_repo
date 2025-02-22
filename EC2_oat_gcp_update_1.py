e382d1a10d96ffc2a083cb248de262246a0d2708
Pavan-lanka <pavan.lanka@azistaindustries.com>
2025-02-22 17:02:28 +0530

    Changes to multiplefiles

--- EC2_oat_gcp_update_1.py
+++ EC2_oat_gcp_update_1.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import pandas as pd
 import psycopg2
 from sshtunnel import SSHTunnelForwarder
 
@@ -11,6 +12,7 @@ SSH_USER = 'ubuntu'
 SSH_KEY_PATH = os.path.expanduser('~/Documents/web_appInstance/HHCL-Ubuntu-key.pem')
 gcp_dir = os.path.expanduser('/mnt/nas_analytics/Annotation/AFR_GCP/GCP_files')
 oat_dir = os.path.expanduser('/mnt/data_products/afr/processed_data/oat')
+ref_dir = os.path.expanduser('/mnt/data_products/afr/processed_data/reference_data/merged_ref')
 
 POSTGRES_HOST = '127.0.0.1'
 POSTGRES_DB = 'web_application_production'
@@ -24,6 +26,7 @@ POSTGRES_PORT = 5432
 table_name = 'images_data'
 id_column = 'id'
 oat_flag_column = 'oat_flag'
+ref_flag_column = 'ref_flag'
 gcp_flag_column = 'gcp_flag'
 
 
@@ -59,32 +62,59 @@ gcp_flag_column = 'gcp_flag'
 #         print(f"Checking {path}: {os.path.exists(path)}")
     
 #     return result
-def check_id_in_directory(directory, ids):
+# def check_id_in_directory(directory, ids, type=None):
+#     """
+#     Check for the existence of .csv files matching given IDs in the specified directory.
+
+#     Args:
+#         directory (str): The base directory to check.
+#         ids (list): List of IDs to check for.
+
+#     Returns:
+#         dict: A dictionary with IDs as keys and flag values indicating existence.
+#     """
+#     result = {}
+#     for id_ in ids:
+#         base_id = id_
+#         if '_' in id_:
+#             base_id = id_.split('_')[0]
+        
+#         # Use glob to match any .csv file starting with base_id
+#         pattern = os.path.join(directory, f"{base_id}*.csv")
+#         matching_files = glob.glob(pattern)
+        
+#         # Set result to 1 if any matching files are found, else 0
+#         result[id_] = 1 if matching_files else 0
+        
+#     # print(f"Checking {pattern}: {'Found' if matching_files else 'Not found'}")
+    
+#     return result
+def check_id_in_directory(directory, ids, type=None):
     """
-    Check for the existence of .csv files matching given IDs in the specified directory.
+    Check for the existence of files matching given IDs in the specified directory.
 
     Args:
         directory (str): The base directory to check.
         ids (list): List of IDs to check for.
+        type (str, optional): If "ref", checks for .tiff files instead of .csv.
 
     Returns:
         dict: A dictionary with IDs as keys and flag values indicating existence.
     """
     result = {}
+    
+    file_extension = ".tiff" if type == "ref" else ".csv"
+    
     for id_ in ids:
-        base_id = id_
-        if '_' in id_:
-            base_id = id_.split('_')[0]
+        base_id = id_.split('_')[0] if '_' in id_ else id_
         
-        # Use glob to match any .csv file starting with base_id
-        pattern = os.path.join(directory, f"{base_id}*.csv")
+        # Use glob to match any file starting with base_id and ending with the required extension
+        pattern = os.path.join(directory, f"{base_id}*{file_extension}")
         matching_files = glob.glob(pattern)
         
         # Set result to 1 if any matching files are found, else 0
         result[id_] = 1 if matching_files else 0
         
-    # print(f"Checking {pattern}: {'Found' if matching_files else 'Not found'}")
-    
     return result
 
 
@@ -103,6 +133,7 @@ def fetch_ids_from_database():
 
             # Use the local bind port provided by the SSH tunnel
             local_port = tunnel.local_bind_port            # Connect to the PostgreSQL database
+            # local_port = 5432            # Connect to the PostgreSQL database
             conn = psycopg2.connect(
                 database=POSTGRES_DB,
                 user=POSTGRES_USER,
@@ -124,7 +155,7 @@ def fetch_ids_from_database():
     return ids
 
 
-def update_database_with_flags(ids, oat_flags, gcp_flags):
+def update_database_with_flags(ids, oat_flags, gcp_flags, ref_flags):
     try:
         # Create an SSH tunnel for secure connection
         with SSHTunnelForwarder(
@@ -136,8 +167,9 @@ def update_database_with_flags(ids, oat_flags, gcp_flags):
             if not tunnel.is_active:
                 raise Exception("SSH tunnel could not be established.")
 
-            # Use the local bind port provided by the SSH tunnel
-            local_port = tunnel.local_bind_port                
+        # Use the local bind port provided by the SSH tunnel
+        # local_port = tunnel.local_bind_port                
+            local_port = 5432                
             conn = psycopg2.connect(
                 database=POSTGRES_DB,
                 user=POSTGRES_USER,
@@ -147,25 +179,26 @@ def update_database_with_flags(ids, oat_flags, gcp_flags):
             )
             cursor = conn.cursor()
 
-            for id_, oat_flag, gcp_flag in zip(ids, oat_flags, gcp_flags):
+            for id_, oat_flag, gcp_flag, ref_flag in zip(ids, oat_flags, gcp_flags, ref_flags):
                 query = f"""
                 UPDATE {table_name}
-                SET {oat_flag_column} = %s, {gcp_flag_column} = %s
+                SET {oat_flag_column} = %s, {gcp_flag_column} = %s, {ref_flag_column} = %s
                 WHERE {id_column} = %s
                 """
-                cursor.execute(query, (int(oat_flag), int(gcp_flag), id_))
+                cursor.execute(query, (oat_flag, gcp_flag, ref_flag, id_))
 
             conn.commit()
             cursor.close()
             conn.close()
         print("Database updated successfully.")
     except Exception as e:
+        
         print(f"An error occurred: {e}")
 
 
 def ensure_columns_exist():
     try:
-        # Create an SSH tunnel for secure connection
+            # Create an SSH tunnel for secure connection
         with SSHTunnelForwarder(
             (SSH_HOST, SSH_PORT),
             ssh_username=SSH_USER,
@@ -175,8 +208,9 @@ def ensure_columns_exist():
             if not tunnel.is_active:
                 raise Exception("SSH tunnel could not be established.")
 
-            # Use the local bind port provided by the SSH tunnel
+                # Use the local bind port provided by the SSH tunnel
             local_port = tunnel.local_bind_port    
+            # local_port = 5432    
             # Connect to the PostgreSQL database
             conn = psycopg2.connect(
                 database=POSTGRES_DB,
@@ -216,6 +250,20 @@ def ensure_columns_exist():
                 END
                 $$;
             """)
+            # Check and create gcp_flag column
+            cursor.execute(f"""
+                DO $$
+                BEGIN
+                    IF NOT EXISTS (
+                        SELECT 1
+                        FROM information_schema.columns
+                        WHERE table_name = '{table_name}' AND column_name = '{ref_flag_column}'
+                    ) THEN
+                        ALTER TABLE {table_name} ADD COLUMN {ref_flag_column} INT DEFAULT 0;
+                    END IF;
+                END
+                $$;
+            """)
 
             conn.commit()
             cursor.close()
@@ -235,10 +283,18 @@ if __name__ == "__main__":
     # Check the directories and files for oat and gcp flags
     oat_flags = check_id_in_directory(oat_dir, ids)
     gcp_flags = check_id_in_directory(gcp_dir, ids)
+    ref_flags = check_id_in_directory(ref_dir, ids, type='ref')
 
     # Prepare the flag lists for database update
     oat_flag_list = [oat_flags[id_] for id_ in ids]
     gcp_flag_list = [gcp_flags[id_] for id_ in ids]
+    ref_flag_list = [ref_flags[id_] for id_ in ids]
+    df = pd.DataFrame({
+        'ID': ids,
+        'oat_flag': [oat_flags[id_] for id_ in ids],
+        'gcp_flag': [gcp_flags[id_] for id_ in ids],
+        'ref_flag': [ref_flags[id_] for id_ in ids]
+    })
 
-    # Update the database with the flags
-    update_database_with_flags(ids, oat_flag_list, gcp_flag_list)
+    df.to_csv("flags.csv", index=False)
+    update_database_with_flags(ids, oat_flag_list, gcp_flag_list, ref_flag_list)
